d5133989ef84410d17589ddb21b2fdae1d2cc870
Matti Laakso <matti.laakso@outlook.com>
2016-07-11 15:45:36 +0300

    powermon: Add InfluxDB client

---
+++ InfluxdbWriter.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include "InfluxdbWriter.h"
+
+namespace PowerMonitor
+{
+
+InfluxdbWriter::InfluxdbWriter(const std::string& host, unsigned int port, const std::string& db, const std::string& user, const std::string& password) :
+        _running(0)
+{
+    CURLcode ret;
+    struct curl_slist *list = NULL;
+    ret = curl_global_init(CURL_GLOBAL_ALL);
+    if (ret)
+    {
+        throw std::runtime_error("Could not initialize curl: "
+                + std::string(curl_easy_strerror(ret)));
+    }
+    _handle = curl_easy_init();
+    if (!_handle)
+    {
+        curl_global_cleanup();
+        throw std::runtime_error("Unable to obtain curl handle.");
+    }
+    _multihandle = curl_multi_init();
+    if (!_multihandle)
+    {
+        curl_easy_cleanup(_handle);
+        curl_global_cleanup();
+        throw std::runtime_error("Unable to obtain curl multi handle.");
+    }
+
+    std::stringstream url;
+    url << "http://" << host << ":" << port << "/write?db=" << db << "&u=" << user << "&p=" << password;
+    curl_easy_setopt(_handle, CURLOPT_URL, url.str().c_str());
+    list = curl_slist_append(list, "Expect:");
+    curl_easy_setopt(_handle, CURLOPT_HTTPHEADER, list);
+    curl_multi_add_handle(_multihandle, _handle);
+    curl_multi_perform(_multihandle, &_running);
+}
+
+InfluxdbWriter::~InfluxdbWriter()
+{
+    curl_multi_remove_handle(_multihandle, _handle);
+    curl_easy_cleanup(_handle);
+    curl_multi_cleanup(_multihandle);
+    curl_global_cleanup();
+}
+
+void InfluxdbWriter::_doSend()
+{
+    curl_multi_perform(_multihandle, &_running);
+    if (!_running)
+    {
+        std::stringstream payload;
+        for (auto&& line : _buffer)
+        {
+            payload << line << "\n";
+        }
+        _buffer.clear();
+        curl_multi_remove_handle(_multihandle, _handle);
+        curl_easy_setopt(_handle, CURLOPT_POSTFIELDS, payload.str().c_str());
+        curl_multi_add_handle(_multihandle, _handle);
+        curl_multi_perform(_multihandle, &_running);
+    }
+}
+
+}
---
+++ InfluxdbWriter.h
@@ -0,0 +1,102 @@
+#ifndef INFLUXDBWRITER_H_
+#define INFLUXDBWRITER_H_
+
+#include <chrono>
+#include <map>
+#include <memory>
+#include <deque>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <curl/curl.h>
+
+namespace PowerMonitor
+{
+//! InfluxDB client.
+
+//!
+//! Sends measurements to InfluxDB. Supports integers, floats and strings,
+//! and string tags. Time stamps can be provided or generated on the fly.
+//!
+class InfluxdbWriter
+{
+public:
+    //! Constructor
+    InfluxdbWriter(const std::string& host, unsigned int port, const std::string& db, const std::string& user, const std::string& password);
+    //! Destructor
+    ~InfluxdbWriter();
+    //! Send a measurement to InfluxDB.
+    //! \param measurement measurement to send to.
+    //! \param values map of field-value-pairs.
+    //! \param timestamp timestamp in nanoseconds since Unix epoch.
+    template<class T> void send(
+            const std::string& measurement,
+            const std::map<std::string, T>& values,
+            long long timestamp = std::chrono::system_clock::now().time_since_epoch().count())
+    {
+        std::stringstream ss;
+        ss << measurement << " ";
+        for (auto&& p : values)
+        {
+            if (!std::is_arithmetic<T>::value)
+                ss << p.first << "=\"" << p.second << "\",";
+            else if (std::is_integral<T>::value)
+                ss << p.first << "=" << p.second << "i,";
+            else
+                ss << p.first << "=" << p.second << ",";
+        }
+        // Replace the last comma with a whitespace.
+        ss.seekp(-1, ss.cur);
+        ss << " " << timestamp;
+        _buffer.push_back(ss.str());
+        _doSend();
+    }
+    //! Send a measurement to InfluxDB.
+    //! \param measurement measurement to send to.
+    //! \param values map of field-value-pairs.
+    //! \param tags map of key-value-pairs.
+    //! \param timestamp timestamp in nanoseconds since Unix epoch.
+    template<class T> void send(
+            const std::string& measurement,
+            const std::map<std::string, T>& values,
+            const std::map<std::string, std::string>& tags,
+            long long timestamp = std::chrono::system_clock::now().time_since_epoch().count())
+    {
+        std::stringstream ss;
+        ss << measurement << ",";
+        for (auto&& p : tags)
+        {
+            ss << p.first << "=" << p.second << ",";
+        }
+        // Replace the last comma with whitespace.
+        ss.seekp(-1, ss.cur);
+        ss << " ";
+        for (auto&& p : values)
+        {
+            if (!std::is_arithmetic<T>::value)
+                ss << p.first << "=\"" << p.second << "\",";
+            else if (std::is_integral<T>::value)
+                ss << p.first << "=" << p.second << "i,";
+            else
+                ss << p.first << "=" << p.second << ",";
+        }
+        // Replace the last comma with whitespace.
+        ss.seekp(-1, ss.cur);
+        ss << " " << timestamp;
+        _buffer.push_back(ss.str());
+        _doSend();
+    }
+
+private:
+    CURLM*                          _multihandle;
+    CURL*                           _handle;
+    std::deque<std::string>         _buffer;
+    int                             _running;
+
+    void _doSend();
+
+};
+
+}
+
+#endif /* INFLUXDBWRITER_H_ */
